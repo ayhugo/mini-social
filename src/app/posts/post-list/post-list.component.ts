95e1e6bf0aedc5cedf9f69e0e168fded928f61f4
MacBook Ayre <hugo@MacBooks-MBP.student.uod.otago.ac.nz>
2019-08-20 14:09:05 +1200

    frontend done. working on backend

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit{
+  // posts = [ {title: 'first post', content: 's1st content'}, {title: '2nd post', content: 's1st content'}];
+  posts: Post[] = [];
+  private postsSub: Subscription;
+
+  constructor(public postsService: PostsService) { }
+
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdatedListener()
+    .subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
+}
