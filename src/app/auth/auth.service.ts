9ca581c2ae1d96aa285fd40ec4396ea7e7e3a6d4
MacBook Ayre <hugo@MacBooks-MacBook-Pro.local>
2019-11-15 16:35:09 +1300

    handled a couple of errors

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -32,10 +32,12 @@ export class AuthService {
 
   createUser(email: string, password: string) {
     const authData: AuthData = {email: email, password: password};
-    this.http.post('http://localhost:3000/api/user/signup', authData)
-      .subscribe(res => {
-        console.log(res);
-      });
+    return this.http.post('http://localhost:3000/api/user/signup', authData)
+    .subscribe(() => {
+      this.router.navigate(['/']);
+    }, error => {
+      this.authStatusListener.next(false);
+    });
   }
 
   login(email: string, password: string){
@@ -55,6 +57,8 @@ export class AuthService {
           this.saveAuthData(token, expirationDate, this.userId);
           this.router.navigate(['/']);
         }
+      }, error => {
+        this.authStatusListener.next(false);
       });
   }
 
